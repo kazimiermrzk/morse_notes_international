3dbc8d71543203177d922000dac70816551eb4b6
David v/d Linde <davidvanderlinde123@gmail.com>
2025-12-04 12:43:22 +0100

    FEAT: added measurement function

--- src/morse_notes_international/model_interperter.py
+++ src/morse_notes_international/model_interperter.py
@@ -16,10 +16,19 @@ def device_list():
 class PhotoResistor:
     # measures amout of light being absorbd by the Photo resistor
 
-    def __init__(self, port):
+    def __init__(self):
         """Connect with the Arduino.
 
         Args:
             port (str): The port to which the Arduino is connected.
         """
-        self.device = ArduinoVISADevice(port=port)
+        self.device = ArduinoVISADevice(port="ASRL9::INSTR")
+
+    def scan(self):
+        self.device.set_output_value(value=1023)
+        print(self.device.get_input_value(2))
+
+
+print(device_list())
+resistor = PhotoResistor()
+resistor.scan()
