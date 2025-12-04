5c08639791de8e1756fc25bbc20061657a3ba56f
David v/d Linde <davidvanderlinde123@gmail.com>
2025-12-04 12:16:02 +0100

    FEAT:  started working on model

---
+++ src/morse_notes_international/model_interperter.py
@@ -0,0 +1,25 @@
+from morse_notes_international.arduino_interperter import (
+    ArduinoVISADevice,
+    resource_list,
+)
+
+
+def device_list():
+    """creates a list of connected ports
+    Returns:
+    list: A list of connected ports."""
+
+    ports = resource_list()
+    return ports
+
+
+class PhotoResistor:
+    # measures amout of light being absorbd by the Photo resistor
+
+    def __init__(self, port):
+        """Connect with the Arduino.
+
+        Args:
+            port (str): The port to which the Arduino is connected.
+        """
+        self.device = ArduinoVISADevice(port=port)
