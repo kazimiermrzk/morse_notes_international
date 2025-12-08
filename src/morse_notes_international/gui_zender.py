c9dbae4ccd3ffdc91a9f1fd5e37ac852ed463763
Zakariya Marzouk <zakariya@marzouk.nl>
2025-12-08 14:27:56 +0100

    code met alfabet

---
+++ src/morse_notes_international/gui_zender.py
@@ -0,0 +1,6 @@
+from morse_notes_international.model_zender import DiodeExperiment
+
+import sys
+from PySide6 import QtWidgets
+from PySide6.QtCore import Slot
+import pyqtgraph as pg
--- src/morse_notes_international/model_zender.py
+++ src/morse_notes_international/model_zender.py
@@ -4,19 +4,49 @@ from morse_notes_international.arduino_zender import (
     list_resources,
     converting_ADC,
 )
+import time
 
 
 class DiodeExperiment:
     def __init__(self):
-        self.device = ArduinoVISADevice(port="ASRL13::INSTR")
+        self.device = ArduinoVISADevice(port="ASRL15::INSTR")
 
     def scan(self):
-        self.device.set_output_value(1000)
+        # self.device.set_output_value(1000)
+
+        morse_beebs = {
+            "a": ".-",
+            "b": "-...",
+            "c": "-.-.",
+            "d": "-..",
+            "e": ".",
+            "f": "..-.",
+            "g": "--.",
+            "h": "....",
+            "i": "..",
+            "j": ".---",
+            "k": "-.-",
+            "l": ".-..",
+            "m": "--",
+            "n": "-.",
+        }
+
+        for a in morse_beebs.values():
+            if a == ".":
+                self.device.set_output_value(1000)
+                time(1)
+
+            if a == "-":
+                self.device.set_output_value(1000)
+                time(2)
+
         self.device.set_output_value(0)
-        self.device.set_output_value(1000)
-        self.device.set_output_value(0)
-        self.device.set_output_value(1000)
-        self.device.set_output_value(0)
+
+        # self.device.set_output_value(0)
+        # self.device.set_output_value(1000)
+        # self.device.set_output_value(0)
+        # self.device.set_output_value(1000)
+        # self.device.set_output_value(0)
 
 
 def LED():
