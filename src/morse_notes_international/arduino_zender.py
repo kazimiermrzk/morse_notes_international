5e52094dcd0e186c228f4411dabf631b23b30c78
David v/d Linde <davidvanderlinde123@gmail.com>
2025-12-08 14:28:57 +0100

    Add continuous measurement to PhotoResistor class
    Introduced an 'on' method to PhotoResistor for repeated light measurements and updated the port for ArduinoVISADevice. Also removed an unused import in arduino_zender.py and added a new (empty) view_interperter.py file.

--- src/morse_notes_international/arduino_zender.py
+++ src/morse_notes_international/arduino_zender.py
@@ -1,6 +1,5 @@
 import pyvisa
 
-
 # rm = pyvisa.ResourceManager("@py")
 
 
--- src/morse_notes_international/model_interperter.py
+++ src/morse_notes_international/model_interperter.py
@@ -1,3 +1,5 @@
+import time as t
+
 from morse_notes_international.arduino_interperter import (
     ArduinoVISADevice,
     resource_list,
@@ -22,13 +24,27 @@ class PhotoResistor:
         Args:
             port (str): The port to which the Arduino is connected.
         """
-        self.device = ArduinoVISADevice(port="ASRL9::INSTR")
+        self.device = ArduinoVISADevice(port="ASRL11::INSTR")
 
     def scan(self):
         self.device.set_output_value(value=1023)
-        print(self.device.get_input_value(2))
+        data = self.device.get_input_value(2)
+        print(data)
+        return data
+
+    def on(self):
+        # measure while turned on
+        data = []
+        i = 0
+        while i <= 1000:
+            i += 1
+            light_score = PhotoResistor.scan(self)
+            data.append(light_score)
+            t.sleep(0.1)
+        return data
 
 
 print(device_list())
 resistor = PhotoResistor()
-resistor.scan()
+a = resistor.on()
+print(a)
